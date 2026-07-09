4136895ec76b2910c4b450d40f0437130a04bbc3
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-09 18:38:24 +0530

    Add OOP classes objects topic

---
+++ 03_oop_approach/01_classes_objects.py
@@ -0,0 +1,14 @@
+"""
+Classes and Objects in Python
+
+A class is a blueprint.
+An object is an instance of a class.
+"""
+
+class Student:
+    def display_info(self):
+        print("Student is learning Python")
+
+
+student1 = Student()
+student1.display_info()
