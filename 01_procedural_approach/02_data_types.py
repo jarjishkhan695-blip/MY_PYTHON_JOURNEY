4fa759312d51704f276fa5e2c8218fa546471815
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-09 18:36:02 +0530

    Add procedural data types topic

---
+++ 01_procedural_approach/02_data_types.py
@@ -0,0 +1,31 @@
+"""
+Data Types in Python
+
+Common built-in data types:
+- int
+- float
+- str
+- bool
+- list
+- tuple
+- dict
+- set
+"""
+
+number = 10
+price = 99.99
+message = "Python is powerful"
+is_active = True
+fruits = ["apple", "banana", "mango"]
+coordinates = (10, 20)
+student = {"name": "Ali", "age": 20}
+unique_numbers = {1, 2, 3, 3}
+
+print(type(number))
+print(type(price))
+print(type(message))
+print(type(is_active))
+print(type(fruits))
+print(type(coordinates))
+print(type(student))
+print(type(unique_numbers))
