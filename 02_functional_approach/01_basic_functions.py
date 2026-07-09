b03bb88aeb8d3dea9f9f280dfadfb2df12eb9cc8
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-09 18:37:17 +0530

    Add functional basic functions topic

---
+++ 02_functional_approach/01_basic_functions.py
@@ -0,0 +1,19 @@
+"""
+Basic Functions in Python
+
+A function is a reusable block of code.
+Functions help avoid repetition.
+"""
+
+def greet():
+    print("Hello, welcome to Python!")
+
+
+def add_numbers():
+    a = 10
+    b = 20
+    print("Sum:", a + b)
+
+
+greet()
+add_numbers()
