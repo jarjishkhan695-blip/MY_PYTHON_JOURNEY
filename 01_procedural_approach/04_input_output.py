98b12270abbf239a9cdefaea98e28054d962e64b
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-09 18:36:19 +0530

    Add procedural input output topic

---
+++ 01_procedural_approach/04_input_output.py
@@ -0,0 +1,14 @@
+"""
+Input and Output in Python
+
+input() is used to take user input.
+print() is used to show output.
+
+Note: input() always returns a string.
+"""
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("Next year you will be", age + 1)
