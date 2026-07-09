a8c04c3694570e363301b9e5f03fe19b7f19ea5e
Khan Jarjish <jarjishkhan695@gmail.com>
2026-07-09 18:35:54 +0530

    Add procedural variables topic

---
+++ 01_procedural_approach/01_variables.py
@@ -0,0 +1,20 @@
+"""
+Variables in Python
+
+A variable is a name used to store data.
+Python does not require you to declare the type manually.
+"""
+
+name = "Jarjish"
+age = 20
+height = 5.9
+is_learning_python = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Python:", is_learning_python)
+
+# Reassigning a variable
+age = 21
+print("Updated age:", age)
